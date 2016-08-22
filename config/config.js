921b9ea888fa81889811353c1361eae9d1afd812
Sanjay Joshi <joshisa@us.ibm.com>
2016-08-22 10:53:11 -0400

    Updates for IBM Bluemix support

--- config/config.js
+++ config/config.js
@@ -39,7 +39,7 @@ if(defaultVisualizations) {
 }
 
 var defaults = {
-  root: path.normalize('/home/vcap/app/'),
+  root: path.normalize('/home/vcap/app'),
   auth: {
     username: argv.username ? '' + argv.username : process.env.LIGHTNING_USERNAME,
     password: argv.password ? '' + argv.password : process.env.LIGHTNING_PASSWORD,
